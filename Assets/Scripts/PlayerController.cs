abb9c0ef87eb53803bb3788453711cd775174477
Varun Sharma <varunk22110@gmail.com>
2020-11-19 23:27:05 +0530

    A Unity 3D game.
    A game in which we are just hitting objects.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float _speed = 20f;
+    private float _turnSpeed = 50f;    
+    private float _horizontalInput;
+    private float _forwardlInput;
+
+    // Update is called once per frame
+    void Update()
+    {
+        _horizontalInput = Input.GetAxis("Horizontal");
+        _forwardlInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * _speed * _forwardlInput);
+        transform.Rotate(Vector3.up, _turnSpeed * _horizontalInput * Time.deltaTime);
+    }
+}
